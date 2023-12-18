22f988fee5de0193dcfb13244a172af92ca8c934
sno0wi <sno0wi20@gmail.com>
2023-12-18 11:30:10 +0200

    clean all js files

--- js/task-01.js
+++
@@ -1,9 +0,0 @@
-const categoryItems = document.querySelectorAll("#categories .item");
-console.log(`Number of categories: ${categoryItems.length}`);
-categoryItems.forEach((item) => {
-    const categoryName = item.querySelector('h2').textContent;
-    console.log(`Category: ${categoryName}`)
-    const categoryElements = item.querySelectorAll('ul > li');
-    console.log(`Elements: ${categoryElements.length}`)
-});
-const categoriesEl = document.querySelector("#categories")
--- js/task-03.js
+++
@@ -1,7 +0,0 @@
-const textInput = document.getElementById("name-input");
-const output = document.getElementById("name-output");
-
-textInput.addEventListener("input", () => {
-    const inputValue = textInput.value.trim();
-    output.textContent = inputValue === "" ? "Anonymous" : inputValue;
-});
